a467672ae567aa5749f1921d859d3e581b47c570
twopercentjazz <chrissnickell@gmail.com>
2023-05-15 20:59:34 -0700

    testing

--- src/test/java/Parser/ParserJunitTests.java
+++ src/test/java/Parser/ParserJunitTests.java
@@ -9,4 +9,22 @@ public class ParserJunitTests {
     void simpleAssert() {
         Assertions.assertEquals(1, 1);
     }
+
+    @Test
+    @DisplayName("Compare Program Output with Given Test Output")
+    void compareOutput() throws IOException {
+        BufferedReader givenOutput = new BufferedReader(new FileReader("src/main/resources/count.par"));
+        BufferedReader parserOutput = new BufferedReader(new FileReader("src/main/resources/loop.par"));
+        String givenLine = givenOutput.readLine();
+        String parserLine = parserOutput.readLine();
+        int line = 1;
+        while(givenLine != null || parserLine != null) {
+            Assertions.assertFalse(givenLine == null || parserLine == null || !givenLine.equals(parserLine));
+            givenLine = givenOutput.readLine();
+            parserLine = parserOutput.readLine();
+            line++;
+        }
+        givenOutput.close();
+        parserOutput.close();
+    }
 }
